7468417b6549a84692fb76292c8b952943a52555
Steven Southwick <steven.southwick@quintiles.com>
2015-12-14 12:56:01 -0500

    building the test data method for unit testing.

---
+++ DataIntegrationTool/BaseClasses/MatchDataItem.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataIntegrationTool.BaseClasses.DynamicHelper;
+
+namespace DataIntegrationTool.BaseClasses
+{
+    public class MatchDataItem : DynamicTypeHelper<MatchDataItem>, ICloneable
+    {
+        private ObservableCollection<MatchDataItem> _matchCollection = new ObservableCollection<MatchDataItem>();
+
+        public ObservableCollection<MatchDataItem> MatchCollection
+        {
+            get { return _matchCollection; }
+
+            set
+            {
+                if (_matchCollection == value)
+                {
+                    return;
+                }
+                _matchCollection = value;
+                RaisePropertyChanged();
+            }
+        }
+        public string MatchKey { get; set; }
+
+        private bool _isMatch;
+
+        public bool IsMatch
+        {
+            get { return _isMatch; }
+
+            set
+            {
+                if (_isMatch == value)
+                {
+                    return;
+                }
+                _isMatch = value;
+                RaisePropertyChanged();
+            }
+        }
+        public decimal MatchScore { get; set; }
+
+        public bool UploadToClinWeb { get; set; } = true;
+
+        public MatchDataItem ShallowCopy()
+        {
+            return (MatchDataItem)MemberwiseClone();
+        }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
---
+++ DataIntegrationToolTests/DataStructures/ImportData.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataIntegrationTool.MainProgram.ImportData;
+using DataIntegrationTool.MessengerPackages;
+using DataIntegrationTool.Resources.Enums;
+
+namespace DataIntegrationToolTests.DataStructures
+{
+    public static class ImportData
+    {
+        public static List<ImportDataPackage> BuildImportDataPackages(bool includeCTMS = true, bool includeBioPharm = true, bool includeClinWeb = true, bool includeOther = true)
+        {
+            var investigatorCollection =
+                ImportDataBLL.GetInvestigationalPerformanceCollection(new Guid("d422355b-6877-48b3-9bf4-83dc7cd18e18"))
+                    .Result;
+            var clinWebDataTable = ImportDataBLL.ConvertClinWebDataToDataView(investigatorCollection);
+            var ctmsDataTable =
+                ImportDataBLL.GetFileData(
+                    @"..\..\TestDataFiles\CTMSTestFile.xlsx").Result;
+            var biopharmDataTable = ImportDataBLL.GetFileData(@"..\..\TestDataFiles\BioPharmTestFile.xlsx").Result;
+            var otherFileDataTable = ImportDataBLL.GetFileData(@"..\..\TestDataFiles\ClinWebTestFile.xlsx").Result;
+
+
+            var clinWebDataPackage = new ImportDataPackage
+            {
+                ClinWebCanvasGuid = new Guid("d422355b-6877-48b3-9bf4-83dc7cd18e18"),
+                FileSource = DataIntegrationTool.Resources.Enums.ImportSource.FileSource.ClinWeb,
+                DataGridToPopulate = "Base",
+                ShortDescription = "Infosario Planning",
+                ImportedData = clinWebDataTable
+            };
+
+            var ctmsDataPackage = new ImportDataPackage
+            {
+                FileSource = ImportSource.FileSource.CTMS,
+                DataGridToPopulate = "ComparerData1",
+                ShortDescription = "CTMS",
+                ImportedData = ctmsDataTable.DefaultView
+            };
+
+            var bioPharmDataPackage = new ImportDataPackage
+            {
+                FileSource = ImportSource.FileSource.BioPharm,
+                DataGridToPopulate = "ComparerData2",
+                ShortDescription = "BioPharm",
+                ImportedData = biopharmDataTable.DefaultView
+            };
+
+            var otherFileDataPackage = new ImportDataPackage
+            {
+                FileSource = ImportSource.FileSource.Other,
+                DataGridToPopulate = "ComparerData3",
+                ShortDescription = "Other",
+                ImportedData = otherFileDataTable.DefaultView
+            };
+
+            var dataPackages = new List<ImportDataPackage>();
+
+            if (includeBioPharm)
+            {
+                dataPackages.Add(bioPharmDataPackage);
+            }
+            if (includeCTMS)
+            {
+                dataPackages.Add(ctmsDataPackage);
+            }
+            if (includeClinWeb)
+            {
+                dataPackages.Add(clinWebDataPackage);
+            }
+            if (includeOther)
+            {
+                dataPackages.Add(otherFileDataPackage);
+            }
+
+            return dataPackages;
+        }
+    }
+}
